462aaa34637c5efd46df94dd59ae799cfd796fd8
webdevorigins <webdevorigins@gmail.com>
2018-07-27 01:32:51 +0530

    Version 0.1 Gaming Events Web Site

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `
+ 	<events-list></events-list>
+  `
+  ,
+ // styleUrls: ['./app.component.css']
+})
+export class EventsAppComponent {
+  title = 'app';
+}
